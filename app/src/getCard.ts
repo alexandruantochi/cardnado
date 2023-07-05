8a13bc38d8c9a41cbe955e426e791ecaab3ec4b9
Alexandru Antochi <alexandru.antochi@outlook.com>
2023-07-05 19:04:14 +0100

    added barcode refresh on store change

--- app/src/getCard.ts
+++ app/src/getCard.ts
@@ -4,6 +4,13 @@ import { resetInfo, setInfo } from "./utils";
 
 declare var JsBarcode: any;
 
+$(document).ready(function() {
+    getCard();
+    $('#store').on('change', function() {
+        getCard();  
+      });
+})
+
 function setBarcodeWidth() {
     $('#barcode').attr('width', '100%');
 }
@@ -11,7 +18,6 @@ function setBarcodeWidth() {
 function generateBarcode(store: string) {
     $.get(constants.getCardUrl, { "store": store })
         .done(function (data) {
-            console.log(data.id);
             JsBarcode("#barcode", data.id);
             setBarcodeWidth();
             resetInfo();
@@ -22,11 +28,11 @@ function generateBarcode(store: string) {
         }).always(() => { $('#get-card').removeClass('disabled'); });
 }
 
+
 function getCard() {
     setInfo('Please wait...', 'default');
     $('#get-card').addClass('disabled');
     const store = $(constants.store).val() as string;
-    console.log(store);
     if (!validateStore(store)) {
         setInfo('Invalid store selected', 'warning');
     }
