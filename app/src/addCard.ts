ce650960a57a746483caca1015b4d557237ccaac
Alexandru Antochi <alexandru.antochi@outlook.com>
2023-07-05 18:18:18 +0100

    improved checkbox ux

--- app/src/addCard.ts
+++ app/src/addCard.ts
@@ -4,27 +4,33 @@ import constants from './constants';
 import { AddCardRequest } from '../../common/models';
 
 const checkboxId = '#accept-checkbox';
+const checkboxLabelId = '#checkbox-label';
 
 
 export async function addCard() {
     const store = $(constants.store).val() as string;
     const cardNumber = $(constants.cardNumber).val() as string;
     const validationResult = validateStoreAndCard(store, cardNumber);
+    const checkbox = $(checkboxId);
+    const checkboxLabel = $(checkboxLabelId);
+    checkboxLabel.removeClass('text-danger');
+
     if (validationResult.valid) {
         const addCardRequest: AddCardRequest = {
             store: store,
             cardNumber: cardNumber
         };
-        if ($(checkboxId).is(':checked')) {
+        if (checkbox.is(':checked')) {
             $.post(constants.addCardUrl, JSON.stringify(addCardRequest))
                 .done(function (data: any) {
                     setInfo(data.message, 'success');
                 })
                 .fail(function (data: any) {
                     setInfo(data.responseJSON.message, 'warning');
-                })
+                });
         } else {
             setInfo('Please accept sharing this.', 'warning');
+            checkboxLabel.addClass('text-danger');
         }
 
     }
