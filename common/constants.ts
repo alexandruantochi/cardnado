48e6131d6ae2569ee629f02126e8bce1176aea92
Alexandru Antochi <alexandru.antochi@outlook.com>
2025-05-15 21:54:44 +0100

    added contact form

--- common/constants.ts
+++ common/constants.ts
@@ -2,6 +2,8 @@ const commonConstants = {
     blobStorageUrl: "https://cardnadone01.blob.core.windows.net",
     blobStorageCardsContainer : "cards",
     blobStorageCardData : "cards.json",
+    blobStorageContactContainer : "data",
+    blobStorageContactData : "feedback.txt"
 }
 
 export { commonConstants }
